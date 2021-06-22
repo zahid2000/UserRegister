f4c21e47add4a0c076c033f5e325889065cc7462
Zahid Mamedov <67357750+zahid2000@users.noreply.github.com>
2021-06-22 08:34:06 +0400

    Initial commit

---
+++ src/main/java/vabiss/com/userRegister/UserRegisterApplication.java
@@ -0,0 +1,13 @@
+package vabiss.com.userRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vabiss/com/userRegister/UserRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package vabiss.com.userRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
